28b17bab30408680ae0a8d512171204d3a11138e
Jonathan Delgado <jadl1997@gmail.com>
2020-02-08 17:45:47 -0500

    boostrap
    boostrap

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class NavBar extends React.Component{
+    render(){
+    return (
+      <div>
+<nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+
+  <ul className="navbar-nav">
+    <li className="nav-item">
+    </li>
+    <li className="nav-item">
+    </li>
+  </ul>
+
+  <span className="navbar-text">
+    Ecmods
+  </span>
+
+</nav>
+    </div>
+    );
+}
+
+}
+
+export default NavBar;
