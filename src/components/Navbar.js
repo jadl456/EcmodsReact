11762a79d00cba5fd63290a3ed6620af7caf8889
Jonathan Delgado <jadl1997@gmail.com>
2020-02-08 18:16:13 -0500

    navbar
    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,22 +3,20 @@ import React from 'react';
 class NavBar extends React.Component{
     render(){
     return (
-      <div>
-<nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-
-  <ul className="navbar-nav">
-    <li className="nav-item">
-    </li>
-    <li className="nav-item">
-    </li>
-  </ul>
-
-  <span className="navbar-text">
-    Ecmods
-  </span>
-
-</nav>
+<nav className="navbar navbar-expand-lg navbar-dark " style={{backgroundColor: "#1f89fd"}}>
+  <a className="navbar-brand" href="/">Ecmods</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div className="navbar-nav">
+      <a className="nav-item nav-link active" href="/">Inicio <span className="sr-only">(current)</span></a>
+      <a className="nav-item nav-link" href="/">Tienda</a>
+      <a className="nav-item nav-link" href="/">Descargas</a>
+      <a className="nav-item nav-link disabled" href="/" aria-disabled="true">Servidores</a>
     </div>
+  </div>
+</nav>
     );
 }
 
