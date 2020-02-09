ba8b10f01ae28f35a7fcbdd3593ab2933a62ea7a
Jonathan Delgado <jadl1997@gmail.com>
2020-02-08 23:56:30 -0500

    fix navbar
    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
 class NavBar extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          menu: false
+        };
+        this.toggleMenu = this.toggleMenu.bind(this);
+      }
+    
+      toggleMenu(){
+        this.setState({ menu: !this.state.menu })
+      }
+    
     render(){
+        const show = (this.state.menu) ? "show" : "" ;
     return (
 <nav className="navbar navbar-expand-lg navbar-dark " style={{backgroundColor: "#1f89fd"}}>
   <a className="navbar-brand" href="/">Ecmods</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+  <button className="navbar-toggler" type="button" onClick={ this.toggleMenu }>
     <span className="navbar-toggler-icon"></span>
   </button>
-  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+  <div className={"collapse navbar-collapse " + show}>
     <div className="navbar-nav">
       <a className="nav-item nav-link active" href="/">Inicio <span className="sr-only">(current)</span></a>
       <a className="nav-item nav-link" href="/">Tienda</a>
