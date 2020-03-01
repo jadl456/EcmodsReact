5b4158e4153d08e3e5572a54b8ed6f242955fe19
Jonathan Delgado <jadl1997@gmail.com>
2020-03-01 12:25:29 -0500

    Api Rest Server Querry
    Api Rest Server Querry

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Inicio from './pages/Inicio';
 import Descargas from './pages/Descargas';
+import Home from './pages/Inicio';
 
 function App() {
   return (
     <Router>
     <Layout>
         <Switch>
+          <Route exact path="/" component={Home} />
           <Route exact path="/inicio" component = {Inicio}/>
           <Route exact path="/descargas" component = {Descargas}/>
         </Switch>
--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,10 +1,73 @@
 import React from 'react';
+// import ReactDOM from "react-dom";
+
+function CharacterCard(props) {
+    const { character } = props;
+  
+    return (
+        <div >
+            <img src= {"https://minotar.net/avatar/"+character+"/25.png"} alt={character}/>
+          {character}
+        </div>
+    );
+  }
 
 class inicio extends React.Component{
+
+    state = {
+        loading: true,
+        error: null,
+        data: {
+            Playerlist: []
+        },
+    };
+
+    componentDidMount(){
+        this.fetchCharacters();
+
+        this.intervalId = setInterval(this.fetchCharacters, 5000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+      }
+
+    fetchCharacters = async () =>{
+        this.setState({loading: true, error: null})
+
+        try{
+        const response = await fetch('https://api.minetools.eu/query/ecmods.craft.gg/25565');
+
+        const data = await response.json();
+
+        this.setState({
+            loading: false,
+            data: data,
+        });
+    } catch(error){
+        this.setState({
+            loading: false,
+            error: error,
+        });
+    }
+    };
+
     render(){
     return (
         <React.Fragment>
-            <p>Proximamente...</p>
+            <div class="container mt-2">
+            <div className="jumbotron">
+                <p><h2>ElectroMods</h2></p>
+            </div>
+            <p>Juadores conectados</p>
+            <ul class="list-group">
+                {this.state.data.Playerlist.map(character =>(
+                    <li class="list-group-item" key={character.id}>
+                        <CharacterCard character={character}/>
+                    </li>
+                ))}
+            </ul>
+            </div>
         </React.Fragment>
     );
 }
