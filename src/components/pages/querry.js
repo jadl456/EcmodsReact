55ad48c2086b81e7a5d00f3264974b6bd41ed2e5
Jonathan Delgado <jadl1997@gmail.com>
2020-03-01 21:09:21 -0500

    ads

---
+++ src/components/pages/querry.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+function CharacterCard(props) {
+    const { character } = props;
+  
+    return (
+        <div >
+            <img src= {"https://minotar.net/avatar/"+character+"/25.png"} alt={character} data-toggle="tooltip" data-placement="bottom" title={character}/>
+          {character}
+        </div>
+    );
+  }
+
+class query extends React.Component{
+
+    state = {
+        loading: true,
+        error: null,
+        data: {
+            Playerlist: []
+        },
+    };
+
+    componentDidMount(){
+        this.fetchCharacters();
+
+        this.intervalId = setInterval(this.fetchCharacters, 5000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+      }
+
+    fetchCharacters = async () =>{
+        this.setState({loading: true, error: null})
+
+        try{
+        const response = await fetch('https://api.minetools.eu/query/ecmods.craft.gg/25565');
+
+        const data = await response.json();
+
+        this.setState({
+            loading: false,
+            data: data,
+        });
+    } catch(error){
+        this.setState({
+            loading: false,
+            error: error,
+        });
+    }
+    };
+
+    render(){
+        return(
+        <ul class="list-group">
+                {this.state.data.Playerlist.map(character =>(
+                    <li class="list-group-item" key={character.id}>
+                        <CharacterCard character={character}/>
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+}
+
+export default query;
--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,56 +1,9 @@
 import React from 'react';
+
+import Querry from '../components/pages/querry';
 // import ReactDOM from "react-dom";
 
-function CharacterCard(props) {
-    const { character } = props;
-  
-    return (
-        <div >
-            <img src= {"https://minotar.net/avatar/"+character+"/25.png"} alt={character}/>
-          {character}
-        </div>
-    );
-  }
-
 class inicio extends React.Component{
-
-    state = {
-        loading: true,
-        error: null,
-        data: {
-            Playerlist: []
-        },
-    };
-
-    componentDidMount(){
-        this.fetchCharacters();
-
-        this.intervalId = setInterval(this.fetchCharacters, 5000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.intervalId);
-      }
-
-    fetchCharacters = async () =>{
-        this.setState({loading: true, error: null})
-
-        try{
-        const response = await fetch('https://api.minetools.eu/query/ecmods.craft.gg/25565');
-
-        const data = await response.json();
-
-        this.setState({
-            loading: false,
-            data: data,
-        });
-    } catch(error){
-        this.setState({
-            loading: false,
-            error: error,
-        });
-    }
-    };
 
     render(){
     return (
@@ -60,14 +13,9 @@ class inicio extends React.Component{
                 <p><h2>ElectroMods</h2></p>
             </div>
             <p>Juadores conectados</p>
-            <ul class="list-group">
-                {this.state.data.Playerlist.map(character =>(
-                    <li class="list-group-item" key={character.id}>
-                        <CharacterCard character={character}/>
-                    </li>
-                ))}
-            </ul>
+            <Querry/>
             </div>
+            
         </React.Fragment>
     );
 }
