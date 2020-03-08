b0e34c1a1e983fa98bcac88105629d99b9756694
Jonathan Delgado <jadl1997@gmail.com>
2020-03-08 13:29:22 -0500

    ads

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -2,16 +2,26 @@ import React from 'react';
 
 import Querry from '../components/pages/querry';
 // import ReactDOM from "react-dom";
+import AdSense from 'react-adsense';
 
 class inicio extends React.Component{
 
     render(){
     return (
+        
         <React.Fragment>
             <div className="container mt-2">
             <div className="jumbotron">
                 <p><h2>ElectroMods</h2></p>
             </div>
+            <AdSense.Google
+            client='ca-pub-8559008179999989'
+            slot='5530225095'
+            style={{ display: 'block' }}
+            format='auto'
+            responsive='true'
+            layoutKey='-gw-1+2a-9x+5c'
+            />
             <p>Juadores conectados</p>
             <Querry/>
             </div>
