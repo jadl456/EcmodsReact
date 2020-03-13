7995aa1bbf477568eccbfd45ebd4532cee189141
jadl456 <jadl1997@gmail.com>
2020-03-13 09:32:25 -0500

    s

--- src/pages/Descargas.js
+++ src/pages/Descargas.js
@@ -18,7 +18,7 @@ class Descargas extends React.Component{
                     <Link type="button" className="btn btn-outline-primary btn-block" to="//www.mediafire.com/file/gp0pise6htt6eeo/mods_1.7.10.rar/file" target="_blank">Mediafire</Link>
                     <Link type="button" className="btn btn-outline-danger btn-block" to="//mega.nz/#!1jp0mAbb!_r9zYgWb4qUqFBKtiKt4NcrwnWQTkMSIJrck6dSZMns" target="_blank">Mediafire</Link>
                 </div>
-                <div className="row mt-2" />
+                <div className="row mt-2 mp-2" />
                 <AdSense.Google
                     client='ca-pub-8559008179999989'
                     slot='9677299766'
