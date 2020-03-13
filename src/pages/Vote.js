114f8099fdb250317fccc2857540a3f88affe67a
Jonathan Delgado <jadl1997@gmail.com>
2020-03-12 23:46:38 -0500

    vote

--- src/pages/Vote.js
+++ src/pages/Vote.js
@@ -14,12 +14,20 @@ class Vote extends React.Component{
                 format='auto'
                 responsive='true'
             />
-                <div className="row mt-2">
+                <div className="row mt-2 mp-2">
                     <Link type="button" to="//stfly.io/Voto" className="btn btn-primary btn-block">Opcion 1</Link>
                     <Link type="button" to="//stfly.io/Votoo" className="btn btn-danger btn-block">Opcion 2</Link>
                     <Link type="button" to="//stfly.io/Voto3" className="btn btn-secondary btn-block">Opcion 3</Link>
                     <Link type="button" to="//stfly.io/Voto4" className="btn btn-info btn-block">Opcion 4</Link>
                 </div>
+
+                <AdSense.Google
+                client='ca-pub-8559008179999989'
+                slot='9677299766'
+                style={{ display: 'block' }}
+                format='auto'
+                responsive='true'
+            />
             </main>
         )
     }
