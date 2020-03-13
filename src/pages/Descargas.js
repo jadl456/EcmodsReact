5e9ce1fb440c818ae12e4f65d3d9a1f0f3997ac9
Jonathan Delgado <jadl1997@gmail.com>
2020-03-12 23:39:29 -0500

    vita

--- src/pages/Descargas.js
+++ src/pages/Descargas.js
@@ -7,6 +7,7 @@ class Descargas extends React.Component{
     render(){
         return (
             <main role="main" className="container mt-2">
+                <div className="row" >
                 <AdSense.Google
                     client='ca-pub-8559008179999989'
                     slot='9677299766'
@@ -14,10 +15,12 @@ class Descargas extends React.Component{
                     format='auto'
                     responsive='true'
                 />
+                </div>
                 <div className="row mt-2">
                     <Link type="button" className="btn btn-outline-primary btn-block" to="//www.mediafire.com/file/gp0pise6htt6eeo/mods_1.7.10.rar/file" target="_blank">Mediafire</Link>
                     <Link type="button" className="btn btn-outline-danger btn-block" to="//mega.nz/#!1jp0mAbb!_r9zYgWb4qUqFBKtiKt4NcrwnWQTkMSIJrck6dSZMns" target="_blank">Mediafire</Link>
                 </div>
+                <div className="row mt-2" >
                 <AdSense.Google
                     client='ca-pub-8559008179999989'
                     slot='9677299766'
@@ -25,6 +28,7 @@ class Descargas extends React.Component{
                     format='auto'
                     responsive='true'
                 />
+                </div>
             </main>
         );
     }
