9b69442a55a3d5a183c02ac4505829a548585a6a
Jonathan Delgado <jadl1997@gmail.com>
2020-03-21 15:51:39 -0500

    update descargas

--- src/pages/Descargas.js
+++ src/pages/Descargas.js
@@ -18,8 +18,8 @@ class Descargas extends React.Component{
                     responsive='true'
                 />
                 <div className="row my-2">
-                    <Link type="button" className="btn btn-outline-primary btn-block" to="//www.mediafire.com/file/gp0pise6htt6eeo/mods_1.7.10.rar/file" target="_blank">Mediafire</Link>
-                    <Link type="button" className="btn btn-outline-danger btn-block" to="//mega.nz/#!1jp0mAbb!_r9zYgWb4qUqFBKtiKt4NcrwnWQTkMSIJrck6dSZMns" target="_blank">Mediafire</Link>
+                    <Link type="button" className="btn btn-outline-primary btn-block" to="//www.mediafire.com/file/mueca9f82hntc9f/mods.rar/file" target="_blank">Mediafire</Link>
+                    <Link type="button" className="btn btn-outline-danger btn-block" to="//mega.nz/#!Y2w3ESjJ!Cpl3ni8qE8-pephIkwet_OLqfb-swwy3iRwNMhEF42w" target="_blank">Mediafire</Link>
                     <Link type="button" className="btn btn-outline-secondary btn-block" to="//files.minecraftforge.net/maven/net/minecraftforge/forge/1.7.10-10.13.4.1614-1.7.10/forge-1.7.10-10.13.4.1614-1.7.10-installer.jar" target="_blank">Forge 1.7.10</Link>
                 </div>
                 <AdSense.Google
